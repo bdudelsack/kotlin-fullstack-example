60c745828ef62ea7650f137841d9d7bb207ac576
Boris Dudelsack <bdudelsack@gmail.com>
2019-12-08 15:25:58 +0100

    Make kotlin-react work

--- frontend/src/main/kotlin/com.bdudelsack.fullstack/Application.kt
+++ frontend/src/main/kotlin/com.bdudelsack.fullstack/Application.kt
@@ -1,6 +1,14 @@
 package com.bdudelsack.fullstack
 
+import react.dom.h1
+import react.dom.render
+import kotlin.browser.document
+
 
 fun main() {
-    console.log("Frontend started")
+    render(document.getElementById("container")) {
+        h1 {
+            + "Kotlin fullstack example"
+        }
+    }
 }
